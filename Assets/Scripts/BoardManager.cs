d2d98730e2d556130279741b6ef9cea615c80f3d
duynt <duynt1651998@gmail.com>
2021-03-01 15:35:27 +0700

    add moving, simple matching

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public static BoardManager Instance;
+
+    public int column;
+    public int row;
+
+    public GameObject[,] candyPosition;
+
+    public List<GameObject> candyType = new List<GameObject>();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    void Start()
+    {
+        candyPosition = new GameObject[row, column];
+        CreateBoard();
+        ScanForMatches();
+    }
+
+    public void CreateBoard()
+    {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                int random = Random.Range(0, candyType.Count);
+                Vector2 tempPosition = new Vector2(i, j);
+                GameObject newCandy = Instantiate(candyType[random], tempPosition, Quaternion.identity);
+
+                newCandy.GetComponent<Candy>().atRow = j;
+                newCandy.GetComponent<Candy>().atColumn = i;
+                newCandy.transform.parent = this.transform;
+                candyPosition[i, j] = newCandy;
+            }
+        }
+    }
+
+    public void ScanForMatches()
+    {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                if (candyPosition[i, j] != null)
+                {
+                    if (i >= 0 && i < row - 2)
+                    {
+                        if (candyPosition[i + 1, j] != null && candyPosition[i + 2, j] != null)
+                        {
+                            if (candyPosition[i + 1, j].name == candyPosition[i, j].name && candyPosition[i + 2, j].name == candyPosition[i, j].name)
+                            {
+                                candyPosition[i, j].GetComponent<Candy>().isMatched = true;
+                                candyPosition[i + 1, j].GetComponent<Candy>().isMatched = true;
+                                candyPosition[i + 2, j].GetComponent<Candy>().isMatched = true;
+                            }
+                        }
+                    }
+
+                    if (j >= 0 && j < column - 2)
+                    {
+                        if (candyPosition[i, j + 1] != null && candyPosition[i, j + 2] != null)
+                        {
+                            if (candyPosition[i, j + 1].name == candyPosition[i, j].name && candyPosition[i, j + 2].name == candyPosition[i, j].name)
+                            {
+                                candyPosition[i, j].GetComponent<Candy>().isMatched = true;
+                                candyPosition[i, j + 1].GetComponent<Candy>().isMatched = true;
+                                candyPosition[i, j + 2].GetComponent<Candy>().isMatched = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Candy.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candy : MonoBehaviour
+{
+    public int atColumn;
+    public int atRow;
+    public bool isMatched = false;
+
+    private Vector2 firstTouch;
+    private Vector2 finalTouch;
+
+    private float distanceX;
+    private float distanceY;
+    private float minDistance = 0.5f;
+
+    private GameObject otherCandy;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        CheckMatches();
+    }
+    private void OnMouseDown()
+    {
+        firstTouch = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseUp()
+    {
+        finalTouch = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CalculateDistance();
+    }
+
+    private void CalculateDistance()
+    {
+        distanceX = Mathf.Abs(firstTouch.x - finalTouch.x);
+        distanceY = Mathf.Abs(firstTouch.y - finalTouch.y);
+
+        if (distanceX > minDistance || distanceY > minDistance)
+        {
+            MoveCandy();
+        }
+    }
+
+    private void MoveCandy()
+    {
+        if (distanceX > distanceY)
+        {
+            //move horizontal
+            if (firstTouch.x > finalTouch.x && atColumn > 0)
+                MoveLeft();
+            if (firstTouch.x < finalTouch.x && atColumn < BoardManager.Instance.column - 1)
+                MoveRight();
+        }
+        else
+        {
+            //move vertical
+            if (firstTouch.y > finalTouch.y && atRow > 0)
+                MoveDown();
+            if (firstTouch.y < finalTouch.y && atRow < BoardManager.Instance.row - 1)
+                MoveUp();
+        }
+
+        BoardManager.Instance.ScanForMatches();
+    }
+
+    private void MoveLeft()
+    {
+        otherCandy = BoardManager.Instance.candyPosition[atColumn - 1, atRow];
+        otherCandy.GetComponent<Candy>().atColumn += 1;
+        this.atColumn -= 1;
+
+        GetPosition();
+        otherCandy.GetComponent<Candy>().GetPosition();
+    }
+    private void MoveRight()
+    {
+        otherCandy = BoardManager.Instance.candyPosition[atColumn + 1, atRow];
+        otherCandy.GetComponent<Candy>().atColumn -= 1;
+        this.atColumn += 1;
+
+        GetPosition();
+        otherCandy.GetComponent<Candy>().GetPosition();
+    }
+    private void MoveUp()
+    {
+        otherCandy = BoardManager.Instance.candyPosition[atColumn, atRow + 1];
+        otherCandy.GetComponent<Candy>().atRow -= 1;
+        this.atRow += 1;
+
+        GetPosition();
+        otherCandy.GetComponent<Candy>().GetPosition();
+    }
+    private void MoveDown()
+    {
+        otherCandy = BoardManager.Instance.candyPosition[atColumn, atRow - 1];
+        otherCandy.GetComponent<Candy>().atRow += 1;
+        this.atRow -= 1;
+
+        GetPosition();
+        otherCandy.GetComponent<Candy>().GetPosition();
+    }
+    public void GetPosition()
+    {
+        BoardManager.Instance.candyPosition[atColumn, atRow] = this.gameObject;
+        transform.position = new Vector2(atColumn, atRow);
+    }
+
+    public void CheckMatches()
+    {
+        if (isMatched)
+        {
+            SpriteRenderer mySprite = GetComponent<SpriteRenderer>();
+            mySprite.color = new Color(0f, 0f, 0f, 0.2f);
+        }
+    }
+}
