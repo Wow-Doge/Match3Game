3cd831b684c840f88a2d6d1a61629427f734e377
duynt <duynt1651998@gmail.com>
2021-03-02 10:31:21 +0700

    init board without match

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -12,7 +12,6 @@ public class BoardManager : MonoBehaviour
     public GameObject[,] candyPosition;
 
     public List<GameObject> candyType = new List<GameObject>();
-    private List<GameObject> possibleCandyType = new List<GameObject>();
 
     private void Awake()
     {
@@ -34,20 +33,18 @@ public class BoardManager : MonoBehaviour
                 Vector2 tempPosition = new Vector2(i, j);
                 int random = Random.Range(0, candyType.Count);
 
-
-                //int maxIterations = 0;
-                //while (MatchesWhenCreate(i, j, candyType[random]))
-                //{
-                //    random = Random.Range(0, candyType.Count);
-                //    maxIterations++;
-                //}
-                //Debug.Log(maxIterations);
-                //maxIterations = 0;
+                int maxIterations = 0;
+                while (CheckMatchInit(i, j, candyType[random]))
+                {
+                    random = Random.Range(0, candyType.Count);
+                    maxIterations++;
+                }
 
                 GameObject newCandy = Instantiate(candyType[random], tempPosition, Quaternion.identity);
                 newCandy.GetComponent<Candy>().atRow = j;
                 newCandy.GetComponent<Candy>().atColumn = i;
                 newCandy.transform.parent = this.transform;
+                newCandy.name = newCandy.name.Replace("(Clone)", "");
                 candyPosition[i, j] = newCandy;
             }
         }
@@ -125,24 +122,24 @@ public class BoardManager : MonoBehaviour
     //    yield return new WaitForSeconds(0.25f);
     //}
 
-    //public bool MatchesWhenCreate(int column, int row, GameObject candy)
-    //{
-    //    if (column > 1)
-    //    {
-    //        if (candyPosition[column - 1, row].name == candy.name && 
-    //            candyPosition[column - 2, row].name == candy.name)
-    //        {
-    //            return true;
-    //        }
-    //    }
-    //    if (row > 1)
-    //    {
-    //        if (candyPosition[column, row - 1].name == candy.name &&
-    //            candyPosition[column, row - 2].name == candy.name)
-    //        {
-    //            return true;
-    //        }
-    //    }
-    //    return false;
-    //}
-} 
+    public bool CheckMatchInit(int column, int row, GameObject candy)
+    {
+        if (column > 1)
+        {
+            if (candyPosition[column - 1, row].name == candy.name &&
+                candyPosition[column - 2, row].name == candy.name)
+            {
+                return true;
+            }
+        }
+        if (row > 1)
+        {
+            if (candyPosition[column, row - 1].name == candy.name &&
+                candyPosition[column, row - 2].name == candy.name)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
