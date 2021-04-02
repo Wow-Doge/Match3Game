b18509d5b999643a0885c9ff510a826c9dc8bab4
duynt <40496666+Wow-Doge@users.noreply.github.com>
2021-04-02 13:47:52 +0700

    test player and enemy

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyManager : MonoBehaviour
+{
+    public int maxHealth = 3000;
+    public int currentHealth;
+
+    private Image image;
+
+    public static EnemyManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    void Start()
+    {
+        currentHealth = maxHealth;
+        image = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        ReduceAlpha();
+    }
+
+    void ReduceAlpha()
+    {
+        Color color = image.color;
+        Debug.Log(currentHealth);
+        float newAlpha = ((3000 - (3000 - currentHealth)) / 3000);
+        color.a = newAlpha;
+        Debug.Log("newAlpha: = " + newAlpha);
+        Debug.Log("alpha: " + color.a);
+        image.color = new Color(color.r, color.g, color.b, color.a);
+    }
+}
